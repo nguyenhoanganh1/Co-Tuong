07e6e27a7178759567ae5cd283c94f14ce3d9437
nguyenhoanganh1 <hoanganh25022000@gmail.com>
2021-07-24 23:04:19 +0700

    Finished

---
+++ DemoAPI/Lib/Entities/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Entities
+{
+    [Table("Chat")]
+    public class Chat
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+        public string UserName { get; set; }
+        public Guid GroupName { get; set; }
+    }
+}
